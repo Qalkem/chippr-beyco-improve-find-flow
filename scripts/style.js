5ce91fb72ef8b46f3bae1bccdc4cdb7000e55488
Qalkem <76013244+Qalkem@users.noreply.github.com>
2021-12-12 13:32:26 +0100

    filter
    Filter added witgh summary , values , an x-index for the silde of a mnegative margin with a transition on a transform

---
+++ scripts/style.js
@@ -0,0 +1,8 @@
+const FilterButton= document.getElementById('FilterButton');
+const Filter= document.getElementById('Filter')
+FilterButton.addEventListener('click',() => {
+   
+    Filter.classList.toggle('Open') ;
+});
+
+ 
