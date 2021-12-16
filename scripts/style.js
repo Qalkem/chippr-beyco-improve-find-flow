bb01328996b40dba314a7f32c1af76a9dc515e2a
Qalkem <76013244+Qalkem@users.noreply.github.com>
2021-12-16 13:28:58 +0100

    filter toegepast
    filter toegepast  met behulp van de lementen summary details input label en legend . ik heb in de form verschillende form elementen in. Verder heb ik gezorgd dat er een type="checkbox" is voor meerderer opties .

--- scripts/style.js
+++ scripts/style.js
@@ -1,8 +1,18 @@
 const FilterButton= document.getElementById('FilterButton');
+const CloseButton= document.getElementById('CloseButton');
 const Filter= document.getElementById('Filter')
 FilterButton.addEventListener('click',() => {
    
-    Filter.classList.toggle('Open') ;
+    Filter.classList.add('Open') ;
 });
+CloseButton.addEventListener('click',() => {
+   
+    Filter.classList.remove('Open') ;
+});
+
+
+
+
+
 
  
